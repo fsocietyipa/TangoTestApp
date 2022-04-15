f4b56c554a0225119765f31c82ef2a10fae0238c
Okzhetpes <okhzetpes.20@gmail.com>
2022-04-15 19:18:34 +0200

    Initial Commit

---
+++ TangoTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TangoTestApp
+//
+//  Created by fsociety.2 on 15.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
